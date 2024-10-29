909d7a341789c0000c0a12f7957661aa9e07b209
Nishith <nishithsarma@gmail.com>
2024-10-29 13:24:45 -0500

    api diff

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://render-demo-j7ch.onrender.com/api",
 });
 
 // Automatically add token to headers if it exists
